3b412445d18a01a7c877b61940cf74245f064df2
Emily Dittmer <emilyedittmer@gmail.com>
2019-08-09 11:11:21 -0600

    Add error page

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { Route, Switch } from "react-router-dom";
 import HomePage from '../HomePage/HomePage';
+import Error from '../Error/Error'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route render={Error} />
       </Switch>
     </div>
   );
